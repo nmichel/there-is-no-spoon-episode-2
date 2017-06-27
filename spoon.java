f5f2c4bb029b176dd0fee86c5bd0ce6f0080e57a
Nicolas Michel <nmichel@smart-trade.net>
2017-06-27 15:32:28 +0200

    Make java version work

--- spoon.java
+++ spoon.java
@@ -118,8 +118,27 @@ class Player {
         }
     }
 
+    static class Context {
+        public Context(final int id, final Link op, final Node node, final int startPos, final int stopPos) {
+            this.id = id;
+            this.op = op;
+            this.node = node;
+            this.startPos = startPos;
+            this.stopPos = stopPos;
+        }
+
+        public final int id;
+        public final Link op;
+        public final Node node;
+        public int startPos;
+        public final int stopPos;
+    }
+
     private final Graph graph;
     private final boolean[] activeNodes;
+    private int ctxtNextId = 0;
+    private final Stack<Context> stack = new Stack<Context>();
+    private final Link[] choices = new Link[31*31*4];
 
     public Player(final Graph graph) {
         this.graph = graph;
@@ -133,19 +152,182 @@ class Player {
         return activeNodes[node.id];
     }
 
+    private void dumpStack() {
+        final HashMap<Integer, Integer> res = new HashMap<>();
+        for (int i = 1; i < stack.size(); ++i) {
+            final Link link = stack.get(i).op;
+            final int minX = Math.min(link.from.x, link.to.x);
+            final int maxX = Math.max(link.from.x, link.to.x);
+            final int minY = Math.min(link.from.y, link.to.y);
+            final int maxY = Math.max(link.from.y, link.to.y);
+            final int k = (minY * 40 + minX) * 10000 + (maxY * 40 + maxX);
+            if (res.get(k) != null) {
+                res.replace(k, 2);
+            }
+            else {
+                res.put(k, 1);
+            }
+        }
+
+        res.entrySet().stream().forEach(e -> {
+            int k = e.getKey();
+            final int minX = (k / 10000) % 40;
+            final int minY = (k / 10000) / 40;
+            final int maxX = (k % 10000) % 40;
+            final int maxY = (k % 10000) / 40;
+            System.out.println(String.format("%d %d %d %d %d", minX, minY, maxX, maxY, e.getValue()));
+        });
+    }
+
+    boolean crossH(final Link operation) {
+        for (int i = 1; i < stack.size(); ++i) {
+            final Link op = stack.get(i).op;
+            if (op.from.y != op.to.y) {
+                continue; // <==  not an horizontal
+            }
+            final int refY = op.from.y;
+            final int minY = Math.min(operation.from.y, operation.to.y);
+            final int maxY = Math.max(operation.from.y, operation.to.y);
+            if (refY <= minY || refY >= maxY) {
+                continue; // <==
+            }
+
+            final int refX = operation.from.x;
+            final int minX = Math.min(op.from.x, op.to.x);
+            final int maxX = Math.max(op.from.x, op.to.x);
+            if (refX <= minX || refX >= maxX) {
+                continue; // <==
+            }
+
+            return true; // <== found one edge crossing
+        }
+        return false;
+    }
+
+    boolean crossV(final Link operation) {
+        for (int i = 1; i < stack.size(); ++i) {
+            final Link op = stack.get(i).op;
+            if (op.from.x != op.to.x) {
+                continue; // <==  not an horizontal
+            }
+
+            final int refX = op.from.x;
+            final int minX = Math.min(operation.from.x, operation.to.x);
+            final int maxX = Math.max(operation.from.x, operation.to.x);
+            if (refX <= minX || refX >= maxX) {
+                continue; // <==
+            }
+
+            final int refY = operation.from.y;
+            final int minY = Math.min(op.from.y, op.to.y);
+            final int maxY = Math.max(op.from.y, op.to.y);
+            if (refY <= minY || refY >= maxY) {
+                continue; // <==
+            }
+
+            return true; // <== found one edge crossing
+        }
+        return false;
+    }
+
+    boolean cross(final Link operation) {
+        final int dir = operation.dir;
+        if (dir == TOP || dir == BOTTOM) {
+            return crossH(operation);
+        }
+        return crossV(operation);
+    }
+
+    boolean commitOperation(final Link link) {
+        if (link.from.current == link.from.target || link.to.current == link.to.target) {
+            return false; // <== source and/or target node are "full"
+        }
+
+        if (cross(link)) {
+            return false; // <== operation would cross a edge
+        }
+
+        ++link.from.current;
+        ++link.to.current;
+
+        return true;
+    }
+
+    void rollbackOperation(final Link link) {
+        --link.from.current;
+        --link.to.current;
+    }
+
+    private int copyChoices(final ArrayList<Link> from, final int pos) {
+        int i = 0;
+        for (; i < from.size(); ++i) {
+            choices[pos+i] = from.get(i);
+        }
+        return pos+i;
+    }
+
     public void play()  {
+        int totalCtxts = 0;
+        int total = graph.total;
+        final long startTime = System.nanoTime();
+        for (int i = 0; i < graph.nodes.size(); ++i) {
+            final Node root = graph.nodes.get(i);
+            final int stopPos = copyChoices(root.choices, 0);
+            activeNodes[root.id] = true;
+            stack.push(new Context(ctxtNextId++, null, root, 0, stopPos));
+            ++totalCtxts;
+            while(total > 0 && !stack.empty()) {
+                final Context ctxt = stack.peek();
+
+                if (ctxt.startPos == ctxt.stopPos) {
+                    if (ctxt.op != null) {
+                        rollbackOperation(ctxt.op);
+                        total += 2;
+                    }
+
+                    if (ctxt.node != null) {
+                        activeNodes[ctxt.node.id] = false;
+                    }
+
+                    stack.pop();
+                    continue; // <==
+                }
+
+                final Link choice = choices[ctxt.startPos++];
+                if (!commitOperation(choice)) {
+                    continue; // <==
+                }
+
+                final Node target = choice.to;
+                Node nextNode = null;
+                int newStopPos;
+                if (isNodeInStack(target)) {
+                    newStopPos = ctxt.stopPos;
+                }
+                else {
+                    nextNode = target;
+                    newStopPos = copyChoices(target.choices, ctxt.stopPos);
+                    activeNodes[target.id] = true;
+                }
+                total -= 2;
+                ++totalCtxts;
+                stack.push(new Context(ctxtNextId++, choice, nextNode, ctxt.startPos, newStopPos));
+            }
+
+            if (total == 0) {
+                System.err.println("Contextes " + totalCtxts);
+                System.err.println("Elapsed " + (System.nanoTime() - startTime));
+                dumpStack();
+                break; // <==
+            }
+            activeNodes[root.id] = false;
+            stack.pop();
+        }
     }
 
     public static void main(String args[]) {
         final Graph graph = Graph.buildGraphFromInput();
         final Player player = new Player(graph);
         player.play();
-
-        // Write an action using System.out.println()
-        // To debug: System.err.println("Debug messages...");
-
-
-        // Two coordinates and one integer: a node, one of its neighbors, the number of links connecting them.
-        System.out.println("0 0 2 0 1");
     }
 }
